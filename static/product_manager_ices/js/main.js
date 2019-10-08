7ab5a8bc04aea6f4196bb30b5f2f08e8b5bf9935
Adam <a.m.przyboroski@gmail.com>
2019-10-08 21:14:13 +0200

    new branch added limitation of ices

--- static/product_manager_ices/js/main.js
+++ static/product_manager_ices/js/main.js
@@ -31,23 +31,34 @@ $(function () {
     var type_ice_s = $("#scoope").prev("input[name='ice']").change(function (event) {
         if (type_ice_s.is(":checked")) {
             $('input[name=flavour]').click(function (event) {
+
                 var input_flavour = $('input[name=flavour]:checked').length;
                 $('.count').attr('value', input_flavour);
 
             });
         }
     });
-    // adding thai ice
+    // adding thai ice limitation on 3 ices
     var type_ice = $("#thai").prev("input[name='ice']").change(function (event) {
         if (type_ice.is(":checked")) {
             $('input[name=flavour]').click(function (event) {
                 $('.count').attr('value', 1);
+                if ($('input[name=flavour]:checked').length >= 3) {
+                    $("input:checkbox:not(:checked)").each(function () {
+                        $(this).prop('disabled', true);
+                    });
+                } else {
+                    $("input:checkbox:not(:checked)").each(function () {
+                        $(this).prop('disabled', false);
+                    });
+                }
 
             });
+
         }
     });
-// reset checkboxe after change
-    $(".radio-toolbar input[name=ice]").change(function (event) {
+// reset checkboxes after change
+    $(".radio-toolbar input[name='ice']").change(function (event) {
         $('input[name=flavour]').prop("checked", false);
         $('.count').attr('value', 1);
     });
@@ -59,14 +70,16 @@ $(function () {
     switch (sts.text()) {
         case "1":
             sts.text('Started');
+            break;
         case "2":
             sts.text('Postponed');
+            break;
         case "3":
-            sts.text('Finished')
-
+            sts.text('Finished');
+            break;
     }
-
-
     // end
+
+
 })
 ;
