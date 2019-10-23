1841a77861409a169407a722ad86f54bd39029ee
Adam <a.m.przyboroski@gmail.com>
2019-10-23 21:12:13 +0200

    added api for list manage orders

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -4,10 +4,8 @@ from django.conf.urls import url
 from .views import AddIceCreateAPIView, AddFlavourCreateAPIView, OrdersListAPIView, OrderChangeView
 
 urlpatterns = [
-    # url(r'^$', OrdersListAPIView.as_view(), name='order-list'),
     url(r'create-ice/$', AddIceCreateAPIView.as_view(), name='create-ice'),
     url(r'add-flavour/$', AddFlavourCreateAPIView.as_view(), name='add-flavour'),
     url(r'order-list/$', OrdersListAPIView.as_view(), name='order-list'),
-    url(r'order-list/(?P<id>(\d)+)$', OrderChangeView.as_view(), name='return-order'),
-
+    url(r'order-manage/(?P<id>(\d)+)$', OrderChangeView.as_view(), name='order-manage'),
 ]
--- drf_api/views.py
+++ drf_api/views.py
@@ -1,56 +1,71 @@
+from django.db.models import Q
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateAPIView, UpdateAPIView, \
-    RetrieveUpdateDestroyAPIView
+    RetrieveUpdateDestroyAPIView, get_object_or_404
 from rest_framework.mixins import UpdateModelMixin
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from drf_api.serializers import AddIcesSerializers, AddFlavourSerializers, OrderListSerializer
 from product_manager_ices.models import Order
 
 
 class AddIceCreateAPIView(CreateAPIView):
+    """
+    endpoint for adding typs of ice
+    """
     permission_classes = [IsAdminUser]
     serializer_class = AddIcesSerializers
 
 
 class AddFlavourCreateAPIView(CreateAPIView):
+    """
+    endpoint for adding flvaoures
+    """
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = AddFlavourSerializers
 
+
 class OrdersListAPIView(ListAPIView):
+    """
+    endpoint list of orders
+    search
+    """
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = OrderListSerializer
 
-    # checkout viewsets or just list and update mixin
+    # simple search
     def get_queryset(self):
-        queryset = Order.objects.filter(worker_owner=self.request.user).order_by("-time_sell")
+        search = self.request.query_params.get('q', None)
+        if search is not None:
+            queryset = Order.objects.filter(Q(worker_owner__username__icontains=search) |
+                                            Q(time_sell__icontains=search) |
+                                            Q(ices_ordered__flavour__flavour__icontains=search) |
+                                            Q(ices_ordered__ice__type__contains=search)).order_by("-time_sell")
+        else:
+            queryset = Order.objects.filter(worker_owner=self.request.user).order_by("-time_sell")
         return queryset
 
+
+# probably better do this with ModelViewSet but it yet to come
 class OrderChangeView(RetrieveUpdateDestroyAPIView):
+    """
+    endpoint changeing order status
+    endpoint for detail of order
+    """
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = OrderListSerializer
     lookup_field = "id"
 
-
+    # setting order to see only for loged user
     def get_queryset(self):
         queryset = Order.objects.filter(worker_owner=self.request.user).order_by("-time_sell")
         return queryset
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
+    # limitation for only one order open
     def perform_update(self, serializer):
         queryset = Order.objects.filter(worker_owner=self.request.user, status=1)
-        if queryset.exists():
+        if queryset.count() > 1:
             raise ValidationError('You have active order opened, Please postpone or delete it')
         serializer.save()
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
