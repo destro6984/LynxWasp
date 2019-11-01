5a8fc70d25f6b979c329a29d8efdc3695900c320
Adam <a.m.przyboroski@gmail.com>
2019-11-01 17:52:33 +0100

    added api order and order item

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -1,7 +1,8 @@
 from django.utils import timezone
 from rest_framework import serializers
+from rest_framework.fields import MultipleChoiceField
 
-from product_manager_ices.models import Ices, Flavour, Order
+from product_manager_ices.models import Ices, Flavour, Order, OrderItem
 
 
 class AddIcesSerializers(serializers.ModelSerializer):
@@ -10,11 +11,12 @@ class AddIcesSerializers(serializers.ModelSerializer):
         fields = "__all__"
 
 
-
 class AddFlavourSerializers(serializers.ModelSerializer):
+
     class Meta:
         model = Flavour
         fields = ("flavour",)
+
 
 
 
@@ -22,6 +24,35 @@ class OrderListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
-        read_only_fields=('ices_ordered','worker_owner','time_sell')
+        read_only_fields = ('ices_ordered', 'worker_owner', 'time_sell')
 
+
+class OrderItemCreateSerializer(serializers.ModelSerializer):
+    # flavour=serializers.MultipleChoiceField(choices=[(flav.id, flav.flavour) for flav in Flavour.objects.all()])
+    # flavour=AddFlavourSerializers(many=True)
+    # ice=serializers.ChoiceField(choices=[(typeice.id, typeice.type) for typeice in Ices.objects.all()])
+    # ice=AddIcesSerializers()
+
+    class Meta:
+        model = OrderItem
+        fields = ["id",'quantity','ice','ice_id','flavour']
+
+    def create(self, validated_data):
+        flavour = validated_data.pop('flavour')
+        orderitem = OrderItem.objects.create(**validated_data)
+        orderitem.flavour.add(*flavour)
+
+        return orderitem
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    # ices_ordered = OrderItemCreateSerializer(many=True,required=False)
+    class Meta:
+        model = Order
+        fields = ["worker_owner", 'time_sell', 'status', 'ices_ordered']
+
+    def create(self, validated_data):
+        ices_ordered = validated_data.pop('ices_ordered')
+        order = Order.objects.create(**validated_data)
+        order.ices_ordered.add(*ices_ordered)
+        return order
 
