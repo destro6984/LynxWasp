aeab20acd0f5e9be3a0ff734430fad1d169e6655
Adam <a.m.przyboroski@gmail.com>
2019-11-13 20:19:10 +0100

    settings fix

--- LynxWasp/settings.py
+++ LynxWasp/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 ALLOWED_HOSTS = ['https://lynxwasp.herokuapp.com/']
 
 # Application definition
