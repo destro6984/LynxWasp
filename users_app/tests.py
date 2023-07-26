cbe9fcd30bd36d3044d2c527e3c3a7c16f6712e8
AdamPrzyboro <adam.przyborowski@stxnext.pl>
2023-07-26 18:40:45 +0200

    IceCreamApp:Tests - firsts testing

--- users_app/tests.py
+++ users_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RegistrationTest(TestCase):
+    username = "tester1"
+    email = "tester1@mail.com"
+
+    def setUp(self):
+        url = reverse("register")
+        self.response = self.client.get(url)
+
+    def test_register_view(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "users_app/register.html")
+        self.assertContains(self.response, "Join Today")
+        self.assertNotContains(self.response, "Lorem Ipsum")
