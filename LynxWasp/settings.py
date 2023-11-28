22a585cb2479d80bdf3373e4f6fc9f4e326b1e9f
AdamPrzyboro <adam.przyborowski@stxnext.pl>
2023-11-28 11:15:38 +0100

    IceCreamApp:RenderCom - new setup

--- LynxWasp/settings.py
+++ LynxWasp/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = "LynxWasp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "structure2.db"),
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
